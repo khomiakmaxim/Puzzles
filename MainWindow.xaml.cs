8ee190e80037886241dcc1b9f6dab20cb08f850f
iammaxymkhomiak <khomiakmaxim@gmail.com>
2020-10-21 19:42:23 +0300

    added mainWindow view

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,28 +1,49 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Microsoft.Win32;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace PuzzlesProj
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for Window1.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
+        public MainWindow()//конструктор головного вікна
         {
-            InitializeComponent();
+            InitializeComponent();//ініціалізуємо головне вікно
+        }
+
+        //private void btnLoad_Click(object sender, RoutedEventArgs e)//хандлер, який спрацьовує при натисканні кнопки
+        //{
+        //    OpenFileDialog op = new OpenFileDialog();//використовуємо цей клас для завантаження фото
+        //    op.Title = "Select a picture";
+        //    op.Filter = "All supported graphics|*.jpg;*.jpeg;*.png|" +
+        //      "JPEG (*.jpg;*.jpeg)|*.jpg;*.jpeg|" +
+        //      "Portable Network Graphic (*.png)|*.png";
+        //    if (op.ShowDialog() == true)//якщо все добре
+        //    {
+        //        imgPhoto.Source = new BitmapImage(new Uri(op.FileName));//беремо картинку з наших файлів
+        //    }
+
+
+        //}
+
+        private void brdWindow_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void grdTop_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+
+        }
+
+        private void grdPuzzle_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+
         }
     }
 }
---
+++ Pixel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PuzzlesProj
+{
+    class Pixel
+    {
+        public int red;
+        public int green;
+        public int blue;
+
+        public double GrayScale()
+        {
+            return (0.3 * red) + (0.59 * green) + (0.11 * blue);
+        }
+
+        public Pixel(int red = 0, int green = 0, int blue = 0)
+        {
+            this.red = red;
+            this.green = green;
+            this.blue = blue;
+        }
+    }
+}
