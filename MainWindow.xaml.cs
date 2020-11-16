e86b1a75f226c0a212546aec2d83b9eb8dea2d81
Andrew Homiak <homiakandrii@gmail.com>
2020-11-16 17:41:17 +0200

    fix removed method call

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -93,7 +93,7 @@ namespace PuzzlesProj
             // TODO: use extension methods to handle conversion
 
             //перетворення bitmapImage в bitmap
-            Bitmap alg = (imageSource);
+            Bitmap alg = BitmapImage2Bitmap(imageSource);
             for (int i = 0; i < rows; ++i)
             {
                 for (int j = 0; j < columns; ++j)
